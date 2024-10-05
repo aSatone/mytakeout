bebe5b6aee33a9fba3c498c9eb5ec71896aba33a
Reii <J973416372@outlook.com>
2024-10-05 21:17:10 +0800

    初始项目

---
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -0,0 +1,21 @@
+package com.sky.mapper;
+
+import com.sky.entity.Employee;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface EmployeeMapper {
+
+    /**
+     * 根据用户名查询员工
+     * @param username
+     * @return
+     */
+    @Select("select * from employee where username = #{username}")
+    Employee getByUsername(String username);
+
+    @Insert("insert into employee(id,name,username,password,phone,sex,id_number,status,create_time,update_time) values (#{id},#{name},#{username},#{password},#{phone},#{sex},#{idNumber},#{status},#{createTime},#{updateTime})")
+    void save(Employee employee);
+}
